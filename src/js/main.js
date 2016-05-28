1e718aab8952c3255fbffa1f5005b898f9b82d2a
rupert <rupert.qing@gmail.com>
2016-05-29 01:06:51 +0800

    basicly ok

--- src/js/main.js
+++ src/js/main.js
@@ -6,19 +6,57 @@ import '../css/app.css'
 import '../../build/css/sprite.css'
 
 
-var noop = function() {}
+const noop = function() {}
 
 
-var app = {
-    init: function() {
+class AnimatePage {
+    constructor() {
+        this.tl = new TimelineLite()
         this.intervalBottle = undefined
         
         // this.writeTrueSize()
-        this.quiverBottle($('.red-bottle:not(.glow)')[0])
-        this.touchBottle($('.red-bottle'), this.redBottleToLeftTop.bind(this))
-    },
+        this.bottlePosture()
+        this.scan()
+            .then(() => {
+                TweenMax.staggerTo([$$('.txt-3'), $$('.txt-2')], 1, {delay: 0.5,opacity: 1}, 0.5);
+                this.quiverBottle($$('.red-bottle'))
+                return this.bindBottleAction($('.red-bottle'), this.redBottleToLeftTop.bind(this))
+            })
+            .then(()=> {
+                TweenMax.to($$('.pipe-red'), 1, {opacity: 1})
+                TweenMax.to($$('.txt-4'), 1, {delay: 0.5,opacity: 1})
+                return this.dropletMove($('.droplet-red-img'))
+            })
+            .then(()=> {
+                return this.girlSmile()
+            })
+            .then(()=> {
+                return this.prepareSecondDroplet()
+            })
+            .then(()=> {
+                TweenMax.to($$('.pipe-red'), 2, {opacity: 0});
+                TweenMax.to($$('.pipe-blue'), 2, {delay: 0.5,opacity: 1})
+                TweenMax.to($$('.txt-8'), 1, {opacity: 1})
+                return this.dropletMove($('.droplet-blue-img'))
+            })
+            .then(()=> {
+                return this.girlBigSmile()
+            })
+    }
     
-    writeTrueSize: function() {
+    scan() {
+        const diff = $$('body').clientHeight - window.innerHeight
+        this.tl.to(window, 0, {delay: 1,scrollTo:{y:0}});
+        this.tl.to(window, 0.5, {scrollTo:{y: diff}, ease: Linear.easeNone});
+        TweenMax.to($$('.red-bottle'), 0.5, {delay: 1.5,scale: 1, y: 30,transformOrigin: "center bottom"});
+        return new Promise((resolve, reject) => {
+            this.tl.to(window, 0.5, {scrollTo:{y:0}, ease: Linear.easeNone, onComplete: ()=> {
+                resolve(1)
+            }});
+        })
+    }
+    
+    writeTrueSize() {
         var urlPat = /url\("?([^"]+)"?\)/
         ;[].slice.apply($('.thing')).forEach(function(el){
             var rawUrlStr = window.getComputedStyle(el,null).getPropertyValue('background-image')
@@ -30,155 +68,162 @@ var app = {
                 el.style.height = e.target.height + 'px'
             }
         })
-    },
+    }
     
-    quiverBottle: function(el, ts) {
+    bottlePosture() {
+        TweenMax.to($$('.red-bottle'), 0, {scale: 0.6, transformOrigin: "center bottom"});
+        TweenMax.to($$('.blue-bottle'), 0, {scale: 0.6, transformOrigin: "center bottom"});
+    }
+    
+    quiverBottle(el, ts) {
+        var self = this
         ts = ts || 0.1
-        var $elGlow = $('.red-bottle.glow')[0]
         function quiverOnce() {
-            TweenMax.to([el, $elGlow], ts, {
-                rotation: -2, 
+            TweenMax.to(el, ts, {
+                rotation: '-=2', 
                 yoyo: true,
                 repeat: 8,
                 onComplete: function() {
-                    TweenMax.to([el, $elGlow], ts, {rotation: 0,transformOrigin: "center bottom"});
+                    TweenMax.to(el, ts, {rotation: '+=2',transformOrigin: "center bottom"});
                 },
                 transformOrigin: "center bottom"});
         }
         quiverOnce()
         this.intervalBottle = setInterval(quiverOnce, 2500)
-    },
+    }
     
-    touchBottle: function(el, callback) {
+    bindBottleAction(el, callback) {
         var self = this
-        
-        // once handler        
-        function handler() {
-            clearInterval(self.intervalBottle)
-            callback && callback()
-            handler = noop
-        }
-        function handlerWrap() {
-            handler()
-        }
-        el.on('touchstart', handlerWrap)
-    },
-    
-    redBottleToLeftTop: function() {
-        var delay = 0.5
-        var $elGlow = $('.red-bottle.glow')[0]
-        var $el = $('.red-bottle:not(.glow)')[0]
-        var $lid = $('.red-bottle-lid')[0]
-        
-        // bottle move to lefttop
-        var aniGlowProp = {
-            opacity: 0, 
-            onComplete: function() {
-                // remove elGlow after animate
-                $elGlow && ($elGlow.outerHTML = '')
+        return new Promise((resolve, reject) => {
+            // once handler        
+            function handler() {
+                clearInterval(self.intervalBottle)
+                callback && callback(resolve)
+                handler = noop
             }
-        }   
+            function handlerWrap() {
+                handler()
+            }
+            el.on('touchstart', handlerWrap)
+        })
+    }
+    
+    redBottleToLeftTop(resolve) {
+        var self = this
+        var delay = 0.5
+        var $el = $$('.red-bottle')
+        var $lid = $$('.red-bottle-lid')
+        
         var aniProp = {
-            x: -564, y: -730,
+            x: -504, y: -570,
             scale: 1.3,
             ease: 'Power4',
             rotation: 150,
             delay: delay,
             transformOrigin: "center bottom"}
-        Object.assign(aniGlowProp, aniProp)     // merge prop
-        TweenMax.to($el, 1, aniProp);
-        TweenMax.to($elGlow, 1, aniGlowProp);
+        this.tl.to($el, 1, aniProp);
         
         // text fade
         TweenMax.staggerTo([$('.txt-3')[0], $('.txt-2')[0]], 1, {delay: 0.5,scale: .7,opacity: 0}, 0.5);
         
         // open lid 
-        TweenMax.to($lid, 0.6, {delay: 1+delay,rotation: 150,
+        this.tl.to($lid, 0.6, {rotation: 150,
             transformOrigin: "right bottom", 
-            onComplete: function() {
-                TweenMax.to($$('.pipe-red'), 1, {opacity: 1})
-                TweenMax.to($$('.txt-4'), 1, {delay: 0.5,opacity: 1})
-                app.dropletMove($$('.droplet-red'), app.girlSmile)
-            }
+            onComplete: resolve
         });
         
-    },
+    }
     
-    dropletMove: function(el, callback) {
+    dropletMove(elArr) {
+        elArr = [].slice.call(elArr)
         $('body')[0].style.overflow = 'hidden'
         TweenMax.to(window, 2, {scrollTo:{y:300}, ease:Power2.easeOut});
         TweenMax.to(window, 2, {delay: 5,scrollTo:{y:800}, ease:Power2.easeOut});
-        
-        TweenMax.to(el, 0.5, {opacity: 1});
-        TweenMax.to(el, 1.5, {delay: .5,x: -156, y: 550,ease: Linear.easeNone});
-        TweenMax.to(el, 1.5, {delay: 2,x: 214, y: 350,ease: Linear.easeNone});
-        TweenMax.to(el, 1.5, {delay: 3.5,x: 314, y: 650,ease: Linear.easeNone});
-        TweenMax.to(el, 1.5, {delay: 5,x: 14, y: 910,ease: Linear.easeNone, onComplete: callback});
-        
-    },
+
+        function moveOne(el, resolve) {
+            var tl = new TimelineMax()
+            tl.to(el, 0.5, {opacity: 1}, 1);
+            tl.to(el, 2.5, {x: -156, y: 550,ease: Linear.easeNone});
+            tl.to(el, 2, {x: 214, y: 350,ease: Linear.easeNone});
+            tl.to(el, 1.5, {x: 314, y: 650,ease: Linear.easeNone});
+            tl.to(el, 1.5, {x: 14, y: 910,ease: Linear.easeNone, onComplete: resolve});
+        }
+        return Promise.all(elArr.map((el, i)=> {
+            return new Promise((resolve, reject)=> {
+                setTimeout(moveOne.bind(this, el, resolve), i*2000)
+            })
+        }))
+    }
     
-    girlSmile: function() {
+    girlSmile() {
+        var self = this
         // $('body')[0].style.overflow = 'auto'
         var $girlSad = $('.girl-sad')
-        TweenMax.to([$('.girl-smile')[0], $('.txt-6')], 1, {opacity: 1});
-        TweenMax.to($('.txt-1'), 1, {opacity: 0});
-        TweenMax.to($girlSad[0], 1, {opacity: 0, onComplete: function() {
-            $girlSad.remove()
-            setTimeout(app.prepareSecondDroplet, 3*1000)
-            }
+        TweenMax.to([$$('.girl-smile'), $$('.txt-6')], 1, {opacity: 1});
+        TweenMax.to($$('.txt-1'), 1, {opacity: 0});
+        return new Promise((resolve, reject)=> {
+            TweenMax.to($girlSad[0], 1, {opacity: 0, onComplete: function() {
+                    $girlSad.remove()
+                    setTimeout(resolve, 1*1000)
+                }
+            });
         });
-    },
+    }
     
-    prepareSecondDroplet: function() {
+    prepareSecondDroplet() {
         $('.red-bottle').remove()
         var $blueBottle = $('.blue-bottle')[0]
-        TweenMax.to($('.pipe-red')[0], 1, {opacity: 0});
         
-        TweenMax.to($blueBottle, 0, {scale: 1.5,transformOrigin: "center bottom"});
-        $blueBottle.style.left = '511px'
-        $blueBottle.style.top = '285px'
+        TweenMax.to($blueBottle, 0, {scale: 1,transformOrigin: "center bottom"});
+        $blueBottle.style.left = '431px'
+        $blueBottle.style.top = '165px'
 
         // hide & show
-        $('.droplet-red, .txt-1, .txt-2, .txt-3, .txt-4,.girl-sad, .red-bottle').remove()
-        ;[]['forEach'].call($('.girl-smile, .txt-6, #scene-3 .bubble'), function (el) {
+        $('.droplet-red, .txt-1, .txt-2, .txt-4,.girl-sad, .red-bottle').remove()
+        ;[]['forEach'].call($('.girl-smile, .pipe-red, .txt-3, .txt-5, .txt-6, #scene-3 .bubble'), function (el) {
             el.style.opacity = 1
         });
-        
         TweenMax.to(window, 2, {scrollTo:{y:0}, ease:Power2.easeOut});
-        app.startBlueDroplet()
-    },
+        this.quiverBottle($$('.blue-bottle'))
+        return this.bindBottleAction($('.blue-bottle'), this.blueBottleToLeftTop.bind(this))
+    }
     
-    startBlueDroplet: function() {
-        app.quiverBottle($$('.blue-bottle'))
-        app.touchBottle($('.blue-bottle'), app.handleBlueBottleTouch)
-    },
+    blueBottleToLeftTop(resolve) {
+        var self = this
+        var delay = 0.5
+        var $el = $$('.blue-bottle')
+        var $lid = $$('.blue-bottle-lid')
+        
+        var aniProp = {
+            x: -504, y: -570,
+            scale: 1.3,
+            ease: 'Power4',
+            rotation: 150,
+            delay: delay,
+            transformOrigin: "center bottom"}
+        this.tl.to($el, 1, aniProp);
+        
+        // text fade
+        TweenMax.staggerTo([$$('.txt-5'), $$('.txt-3')], 1, {scale: .7,opacity: 0}, 0.5);
+        
+        // open lid 
+        this.tl.to($lid, 0.6, {rotation: 150,
+            transformOrigin: "right bottom", 
+            onComplete: resolve
+        });
+        
+    }
     
-    handleBlueBottleTouch: function() {
-        TweenMax.to($$('.blue-bottle'), 1, {opacity: 0})
-        TweenMax.to($$('.blue-bottle-incling'), 1, {
-            delay: 0.5,opacity: 1, 
-            onComplete: function() {
-                TweenMax.to($$('.pipe-blue'), 2, {delay: 0.5,opacity: 1})
-                TweenMax.to($$('.txt-8'), 1, {opacity: 1})
-                app.dropletMove($$('.droplet-blue'), app.girlBigSmile)
-            }
-        })
-    },
+    girlBigSmile() {
+        TweenMax.to([$$('.girl-smile'), $$('.txt-6')], 1, {opacity: 0})
+        TweenMax.to([$$('.girl-big-smile'), $$('.txt-7')], 1, {opacity: 1})
+        setTimeout(this.gotoMall, 5*1000)
+    }
     
-    girlBigSmile: function() {
-        TweenMax.to($$('.droplet-blue'), 1.5, {x: 126, y: 1090});
-        TweenMax.to([$$('.girl-smile'), $$('.txt-6')], 1, {delay: 1,opacity: 0})
-        TweenMax.to([$$('.girl-big-smile'), $$('.txt-7')], 1, {delay: 1,opacity: 1})
-        setTimeout(app.gotoMall, 5*1000)
-    },
-    
-    gotoMall: function() {
-        location.href = 'product.html'
+    gotoMall() {
+        // location.href = 'product.html'
     }
 }
-
-
-
 
 class IndexAnimate {
     constructor() {
@@ -220,14 +265,11 @@ class IndexAnimate {
 }
 
 
-
-
-
 $('body')[0].onload = function() {
     if ($$('body#index-page')) {
         var indexAnimate = new IndexAnimate()
     } else if($$('body#animate-page')) {
-        app.init()
+        window.animatePage = new AnimatePage()
     }
 }
 
